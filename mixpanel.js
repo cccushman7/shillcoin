9aef2f66fb90df08892605ba35dc241ec00ad512
cccushman7 <clark.cushman@mixpanel.com>
2019-07-24 19:12:22 -0400

    Add files via upload

---
+++ mixpanel.js
@@ -0,0 +1,25 @@
+mixpanel.track('Log In', logIn);
+
+
+
+function logOut() {
+	mixpanel.track("Log Out")
+
+}
+
+function memeBreak() {
+
+}
+
+function getRandomName() {
+
+}
+
+function getUserId(user) {
+
+}
+
+function buildCoinPickerDiv(coin) {
+
+
+}
